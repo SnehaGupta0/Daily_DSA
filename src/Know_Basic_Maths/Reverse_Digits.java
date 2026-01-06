19c2cd3f003f9edb75bb21c6718dae77c24b7cc1
Sneha Gupta <snehagupta53586@gmail.com>
2026-01-07 02:51:13 +0530

    Complete

--- src/Know_Basic_Maths/Reverse_Digits.java
+++ src/Know_Basic_Maths/Reverse_Digits.java
@@ -1,6 +1,20 @@
 package Know_Basic_Maths;
 
+import java.util.Scanner;
+
 public class Reverse_Digits
 {
-
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int reverse_num = 0;
+        while(n > 0)
+        {
+            int lastDigit = n % 10;
+            n = n / 10;
+            reverse_num = reverse_num * 10 + lastDigit;
+        }
+        System.out.println(reverse_num);
+    }
 }
