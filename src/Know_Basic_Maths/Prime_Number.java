c65cdb8459f9badd23f897179979374187142df8
Sneha Gupta <snehagupta53586@gmail.com>
2026-01-09 20:28:57 +0530

    Optimal Solution

--- src/Know_Basic_Maths/Prime_Number.java
+++ src/Know_Basic_Maths/Prime_Number.java
@@ -7,13 +7,22 @@ public class Prime_Number
     public static void main(String[] args)
     {
        Scanner sc=new Scanner(System.in);
-       int n=sc.nextInt();
+       int n = sc.nextInt();
        int count = 0;
-       for(int i = 1 ; i <= n ; i++)
+//       for(int i = 1 ; i <= n ; i++)
+//       {
+//           if(n % i == 0)
+//           {
+//               count++;
+//           }
+//       }
+       for(int i = 1 ; i*i <= n ; i++)
        {
-           if(n % i == 0)
-           {
+           if(n %i ==0) {
                count++;
+               if ((n / i) != i) {
+                   count++;
+               }
            }
        }
        if(count == 2)
