25b5b162a7da1cb54f411224c923996821280a7d
Sneha Gupta <snehagupta53586@gmail.com>
2026-01-11 21:46:23 +0530

    Commit

--- src/Learn_Basic_Recursion/Print_Value_N_Times.java
+++ src/Learn_Basic_Recursion/Print_Value_N_Times.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Print_Value_N_Times
 {
+    // ----- 1 to N-----
     public static void PrintNumbers(int n)
     {
         if(n == 0)
@@ -13,6 +14,16 @@ public class Print_Value_N_Times
         PrintNumbers(n-1);
         System.out.println(n);
     }
+    //-----N to 1----
+//    public static void printNumbers(int n)
+//    {
+//        if(n==0)
+//        {
+//            return ;
+//        }
+//        System.out.println(n);
+//        printNumbers(n-1);
+//    }
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
